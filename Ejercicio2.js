ceaec49f396ea3a1ec36651339ecf2f075987f71
DaNyJeReZ <106661035+DaNyXeReZ@users.noreply.github.com>
2022-11-23 16:15:46 +0100

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,25 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const perfil = [
+    "Daniel",
+    39,
+    false,
+    new Date  (1982, 12, 19),
+    {
+       titulo: "Cazadores de sombras",
+       autor : "CASSANDRA CLARE",
+       fecha : 17/02/2009,
+       url : "https://www.casadellibro.com/libro-cazadores-de-sombras-1-ciudad-de-hueso/9788408083801/1236916"
+    }
+]
+
