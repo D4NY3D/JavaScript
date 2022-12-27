1744c996a207f5e7eff976ddd25bb1962d84847a
DaNyJeReZ <106661035+DaNyXeReZ@users.noreply.github.com>
2022-12-27 18:09:36 +0100

    Add files via upload

---
+++ Ejercicio-10/index.js
@@ -0,0 +1,14 @@
+import {suma, multiplica} from "./modulo/controller.js"
+
+import chalk from 'chalk';
+
+
+
+console.log(suma(1,2)),
+console.log(multiplica(4,5));
+
+console.log(chalk.green('Hello world!'));
+
+
+
+
